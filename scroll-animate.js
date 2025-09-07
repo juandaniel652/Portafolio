fa3c1bf81e0b6ba4ad5abf6b9efe35172552274d
Tu Nombre <juankycabj05@gmail.com>
2025-09-07 19:45:41 -0300

    Optimización de velocidad al abrir el sitio

--- scroll-animate.js
+++ scroll-animate.js
@@ -1,31 +1,11 @@
-/* ==============================
-   ANIMACIONES POR SCROLL
-============================== */
-const scrollElements = document.querySelectorAll(".scroll-animate");
+const elements = document.querySelectorAll('.scroll-animate');
 
-const elementInView = (el, dividend = 1) => {
-  const elementTop = el.getBoundingClientRect().top;
-  return elementTop <= (window.innerHeight || document.documentElement.clientHeight) / dividend;
-};
-
-const displayScrollElement = (element) => {
-  element.classList.add("visible");
-};
-
-const hideScrollElement = (element) => {
-  element.classList.remove("visible");
-};
-
-const handleScrollAnimation = () => {
-  scrollElements.forEach((el) => {
-    if (elementInView(el, 1.25)) {
-      displayScrollElement(el);
-    } else {
-      hideScrollElement(el);
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
     }
   });
-};
-
-window.addEventListener("scroll", () => {
-  handleScrollAnimation();
 });
+
+elements.forEach(el => observer.observe(el));
